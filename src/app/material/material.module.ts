c5592f9ac3cdf3e31b55bc60dca56886731c0e92
Maurizio La Rocca <mrz.larocca@gmail.com>
2019-09-11 20:06:36 +0200

    Refactor MaterialModule

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { MatButtonModule, MatIconModule } from '@angular/material';
 
 
+const moduleArray = [
+                      MatButtonModule,
+                      MatIconModule
+                    ];
+
 @NgModule({
-  imports: [MatButtonModule,
-            MatIconModule],
-  exports: [MatButtonModule,
-            MatIconModule]
+  imports: moduleArray,
+  exports: moduleArray
 })
 export class MaterialModule { }
