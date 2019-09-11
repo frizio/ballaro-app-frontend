99740f913f4dd6081f7e6a336c2ae07551eb1c52
Maurizio La Rocca <mrz.larocca@gmail.com>
2019-09-11 22:40:20 +0200

    Add flex layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { FlexLayoutModule } from "@angular/flex-layout";
 import { MaterialModule } from './material/material.module';
 import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -19,7 +20,8 @@ import { SidenavListComponent } from './components/navigation/sidenav-list/siden
     BrowserModule,
     BrowserAnimationsModule,
     AppRoutingModule,
-    MaterialModule
+    MaterialModule,
+    FlexLayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
