dc6fc5a838ee3c064a3383e3a03a97327216851f
Maurizio La Rocca <mrz.larocca@gmail.com>
2019-09-11 20:29:46 +0200

    Add basic routes and routingComponents array

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,22 @@
+import { HomeComponent } from './components/home/home.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MainMapComponent } from './components/main-map/main-map.component';
+import { AboutComponent } from './components/about/about.component';
+import { CreditsComponent } from './components/credits/credits.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'map', component: MainMapComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'credits', component: CreditsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
+export const routingComponents = [HomeComponent, MainMapComponent, AboutComponent, CreditsComponent];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,24 +3,17 @@ import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { MaterialModule } from './material/material.module';
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MainMapComponent } from './components/main-map/main-map.component';
-import { AboutComponent } from './components/about/about.component';
-import { CreditsComponent } from './components/credits/credits.component';
 import { HeaderComponent } from './components/navigation/header/header.component';
 import { SidenavListComponent } from './components/navigation/sidenav-list/sidenav-list.component';
-import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainMapComponent,
-    AboutComponent,
-    CreditsComponent,
     HeaderComponent,
     SidenavListComponent,
-    HomeComponent
+    routingComponents
   ],
   imports: [
     BrowserModule,
