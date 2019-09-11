6fa1b213fd752b5bd4df856b19ac2ce20903ba97
Maurizio La Rocca <mrz.larocca@gmail.com>
2019-09-11 20:17:38 +0200

    Create basic app components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,20 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material/material.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainMapComponent } from './components/main-map/main-map.component';
+import { AboutComponent } from './components/about/about.component';
+import { CreditsComponent } from './components/credits/credits.component';
+import { HeaderComponent } from './components/navigation/header/header.component';
+import { SidenavListComponent } from './components/navigation/sidenav-list/sidenav-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainMapComponent,
+    AboutComponent,
+    CreditsComponent,
+    HeaderComponent,
+    SidenavListComponent
   ],
   imports: [
     BrowserModule,
