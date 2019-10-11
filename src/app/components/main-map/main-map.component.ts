db6ca774ba3aec7b546a0a4ade8aeb2feef8be15
Maurizio La Rocca <mrz.larocca@gmail.com>
2019-10-12 00:06:13 +0200

    Implement layout map component

--- src/app/components/main-map/main-map.component.ts
+++ src/app/components/main-map/main-map.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+declare var ol: any;
+
 @Component({
   selector: 'app-main-map',
   templateUrl: './main-map.component.html',
@@ -7,9 +9,52 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainMapComponent implements OnInit {
 
-  constructor() { }
+  latitude  = 41.6561;
+  longitude = 12.4073;
+
+  map: any;
+
 
   ngOnInit() {
+
+    //
+    this.map = new ol.Map(
+      {
+        target: 'map',
+        layers: [
+          new ol.layer.Tile( { source: new ol.source.OSM() } )
+        ],
+        view:
+          new ol.View(
+            {
+              center: ol.proj.fromLonLat([this.longitude, this.latitude]),
+              zoom: 6
+            }
+          )
+      }
+    );
+
+    //
+    this.map.on(
+      'click',
+      (args: any) => {
+        console.log(args.coordinate);
+        const lonlat = ol.proj.transform(args.coordinate, 'EPSG:3857', 'EPSG:4326');
+        console.log(lonlat);
+        const lon = lonlat[0];
+        const lat = lonlat[1];
+        alert(`latitude: ${lat} longitude: ${lon}`);
+    });
+
+  }
+
+  //
+  setCenter() {
+    const view = this.map.getView();
+    view.setCenter(ol.proj.fromLonLat([this.longitude, this.latitude]));
+    view.setZoom(16);
   }
 
 }
+
+
