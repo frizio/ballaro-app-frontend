9dee46dd54a294eb4b5b14ae1d668d77d92ac4d2
Maurizio La Rocca <maurizio.larocca@exprivia.com>
2019-10-18 17:54:24 +0200

    Add basic layers and overlay

--- src/app/components/main-map/main-map.component.ts
+++ src/app/components/main-map/main-map.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { tileLayer, latLng } from 'leaflet';
+import { tileLayer, latLng, marker, icon } from 'leaflet';
 
 declare var ol: any;
 
@@ -10,17 +10,52 @@ declare var ol: any;
 })
 export class MainMapComponent implements OnInit {
 
-  options = {
-    layers: [
-      tileLayer(
-        'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
-        {maxZoom: 19, attribution: '&copy; OpenStreetMap contributors'}
+  // Define our base layers so we can reference them multiple times
+  streetMaps = tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+                          {
+                            detectRetina: true,
+                            attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                          }
+  );
+
+  wMaps = tileLayer('http://maps.wikimedia.org/osm-intl/{z}/{x}/{y}.png',
+                      {
+                        detectRetina: true,
+                        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                      }
+  );
+
+  // Marker for my home
+  home = marker(
+    [ 38.108, 13.335 ],
+    {
+      icon: icon(
+        {
+          iconSize: [ 25, 41 ],
+          iconAnchor: [ 13, 41 ],
+          iconUrl: 'leaflet/marker-icon.png',
+          shadowUrl: 'leaflet/marker-shadow.png'
+        }
       )
-    ],
+    }
+);
+
+  // Layers control object with our two base layers
+  layersControl = {
+    baseLayers: {
+      'Street Maps': this.streetMaps,
+      'Wikimedia Maps': this.wMaps
+    },
+    overlays: {
+      'My Home': this.home
+    }
+  };
+
+  options = {
+    layers: [ this.streetMaps ],
     zoom: 11,
     center: latLng([38.088, 13.155])
   };
-
   ngOnInit(): void {
     
   }
